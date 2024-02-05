e51a19fbfdd8299273bd20eca0380f9081a3ea12
Zachary Trouy <ztrouy@gmail.com>
2024-02-05 05:26:48 -0600

    Added additional divs for CSS hooks

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,27 @@ import { Kids } from "./Kids.js"
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
-    <h1>Make a Memory for Kids</h1>
-    <article class="details">
-        <section class="detail--column list details__kids">
-            <h2>Kids</h2>
-            ${Kids()}
-        </section>
-        <section class="detail--column details__celebrities">
-            <h2>Celebrities</h2>
-            ${Celebrities()}
-        </section>
-    </article>
-
-    <article class="assignments">
-        <h2>Pairings</h2>
-        ${Pairings()}
-    </article>
+<div id="content">
+    <div class="row">
+        <h1>Make a Memory for Kids</h1>
+        <article class="details">
+            <section class="detail--column list details__kids">
+                <h2>Kids</h2>
+                ${Kids()}
+            </section>
+            <section class="detail--column details__celebrities">
+                <h2>Celebrities</h2>
+                ${Celebrities()}
+            </section>
+        </article>
+    </div>
+    <div class="row">
+        <article class="assignments">
+            <h2>Pairings</h2>
+            ${Pairings()}
+        </article>
+    </div>
+</div>
 `
 
 mainContainer.innerHTML = applicationHTML
